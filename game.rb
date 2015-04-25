ebae2e2a802e65c8b91433353e66f4ef6332ff27
Hal Chen <yalahao@gmail.com>
2015-04-25 05:02:42 +0200

    Added Card

---
+++ game.rb
@@ -0,0 +1,58 @@
+# Modelling
+=begin
+
+Game
+  new
+  start_round
+  compare(hand, hand)
+  play_again_or_quit
+
+Round
+  @current_player
+
+Player
+  @hand
+Human < Player
+  hit_or_stay
+Computer < Player
+  hit_or_stay
+
+Card
+  @suit
+  @rank
+  display
+Deck
+  @cards
+  shuffle
+  deal_to(hand)
+Hand
+  @cards
+  total_score
+  show
+  sort
+
+=end
+
+CLUB = "\u2664 ".encode('utf-8')
+HEART = "\u2661 ".encode('utf-8')
+SPADE = "\u2667 ".encode('utf-8')
+DIAMOND= "\u2662 ".encode('utf-8')
+RANKS = %w{A 2 3 4 5 6 7 8 9 J Q K}
+
+class Card
+  attr_reader :suit, :rank
+
+  def initialize(suit, rank)
+    @suit = suit
+    @rank = rank
+  end
+
+  def to_s
+    "#{suit}#{rank}"
+  end
+end
+
+card = Card.new(DIAMOND, 'A')
+puts card
+
+
